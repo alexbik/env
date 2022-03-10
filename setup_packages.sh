1500a4d6db185b920a8002fc61f007037c70e09b
Alex Bikbaev <bik@google.com>
2022-03-10 12:11:02 +0100

    Change the list of packages in setup_packages.sh and add commands to install YCM

--- setup_packages.sh
+++ setup_packages.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 # Add deps
-sudo aptitude install -y inst build-essential cmake python-dev python3-dev exuberant-ctags
+sudo apt-get install -y aptitude
+sudo aptitude install -y build-essential cmake python3-dev exuberant-ctags vim-nox mono-complete golang nodejs default-jdk npm
+# Install YCM
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py --all
